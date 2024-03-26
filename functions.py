85f9ef3c0974332239240f7f09be34f2f2c86790
Michael Jolley <jolleymc@gmail.com>
2024-03-26 14:57:21 -0400

    Finishing up

--- functions.py
+++ functions.py
@@ -6,63 +6,6 @@ import numpy as np
 import statistics
 
 class Student:
-    def __init__(self, num_classes):
-        self.grades = [random.randint(0, 100) for _ in range(num_classes)]
-
-def generate_grades(num_students, num_classes):
-    return [Student(num_classes) for _ in range(num_students)]
-
-def plot_grades(students):
-    for i, student in enumerate(students):
-        plt.plot(student.grades, label=f'Student {i+1}')
-    plt.legend()
-    plt.show()
-
-def calculate_statistics(students):
-    statistics = []
-    for i in range(len(students[0].grades)):
-        grades = [student.grades[i] for student in students]
-        max_grade = max(grades)
-        min_grade = min(grades)
-        avg_grade = sum(grades) / len(grades)
-        median_grade = statistics.median(grades)
-        std_dev = statistics.stdev(grades)
-        statistics.append((max_grade, min_grade, avg_grade, median_grade, std_dev))
-    return statistics
-
-def plot_statistics(statistics):
-    labels = ['Max', 'Min', 'Avg', 'Median', 'Std Dev']
-    for i, class_statistics in enumerate(statistics):
-        plt.plot(class_statistics, label=f'Class {i+1}')
-    plt.xticks(range(len(labels)), labels)
-    plt.legend()
-    plt.show()
-
-def plot_histogram(students):
-    math_grades = [student.grades[0] for student in students]
-    plt.hist(math_grades, bins=range(101), edgecolor='black')
-    plt.xlabel('Grade')
-    plt.ylabel('Frequency')
-    plt.show()
-
-def main():
-    num_students = 20
-    num_classes = 6
-    students = generate_grades(num_students, num_classes)
-    plot_grades(students)
-    statistics = calculate_statistics(students)
-    plot_statistics(statistics)
-    plot_histogram(students)
-
-if __name__ == "__main__":
-    main()
-
-'''
-import random
-import matplotlib.pyplot as plt
-import numpy as np
-
-class Student:
     def __init__(self):
         self.classAndGrade = {
             'Math': random.randint(0, 100),
@@ -100,26 +43,51 @@ def plotGrades():
     ax.set_title('Average Grade by Subject')
     plt.show()
 
-def calculateStatistics():
+def calculateStatistics(numStudents):
     # Generate grades for a random number of students
     grades = generate_grades(numStudents)
     # Get the list of subjects
     subjects = list(grades[0].classAndGrade.keys())
-    # Initialize a dictionary to store the total grades for each subject
-    averages = {subject: 0 for subject in subjects}
-    # For each student, add their grade for each subject to the total
-    for student in grades:
-        for subject in subjects:
-            averages[subject] += student.classAndGrade[subject]
-    # Calculate the average grade for each subject
+    # Initialize a dictionary to store the statistics for each subject
+    stats = {subject: [] for subject in subjects}
+    # For each subject, calculate the statistics
     for subject in subjects:
-        averages[subject] /= numStudents
-    # Print the average grade for each subject
-    print('Average Grade by Subject:')
+        subject_grades = [student.classAndGrade[subject] for student in grades]
+        stats[subject].append(max(subject_grades))  # Maximum
+        stats[subject].append(min(subject_grades))  # Minimum
+        stats[subject].append(sum(subject_grades) / numStudents)  # Average
+        stats[subject].append(statistics.median(subject_grades))  # Median
+        stats[subject].append(statistics.stdev(subject_grades))  # Standard deviation
+    # Print the statistics for each subject
     for subject in subjects:
-        print(f'{subject}: {averages[subject]}')
-    # Print the total number of students
-    print(f'Total number of students: {numStudents}')
-    # Calculate and print the average GPA (assuming each subject is equally weighted and out of 100)
-    print(f'Average GPA: {np.mean([sum(student.classAndGrade.values()) for student in grades]) / 600}')
-'''
+        print(f'{subject}: Max={stats[subject][0]}, Min={stats[subject][1]}, Avg={stats[subject][2]}, Median={stats[subject][3]}, StdDev={stats[subject][4]}')
+
+def plotStatistics():
+    # Calculate the statistics
+    stats = calculateStatistics(numStudents)
+
+    # Get the list of subjects
+    subjects = list(stats.keys())
+
+    # Create a bar plot for each statistic
+    for subject in subjects:
+        fig, ax = plt.subplots()
+        ax.bar(['Max', 'Min', 'Avg', 'Median', 'StdDev'], stats[subject])
+        ax.set_ylabel('Value')
+        ax.set_title(f'Statistics for {subject}')
+        plt.show()
+
+def plotMathHistogram():
+    # Generate grades for a random number of students
+    grades = generate_grades(numStudents)
+
+    # Get the Math grades for all students
+    math_grades = [student.classAndGrade['Math'] for student in grades]
+
+    # Plot the histogram
+    fig, ax = plt.subplots()
+    ax.hist(math_grades, bins=10)
+    ax.set_xlabel('Math Grade')
+    ax.set_ylabel('Frequency')
+    ax.set_title('Histogram of Math Grades')
+    plt.show()
--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 '''Michael Jolley and Cole Walther - PA 4 - 3/26/2024'''
+
+from functions import generate_grades, plotGrades, calculateStatistics, plotStatistics, plotMathHistogram, numStudents
+
+# Generate grades
+grades = generate_grades(numStudents)
+
+# Plot grades
+plotGrades(grades)
+
+# Calculate statistics
+statistics = calculateStatistics(grades)
+
+# Plot statistics
+plotStatistics(statistics)
+
+# Plot math histogram
+plotMathHistogram(grades)
